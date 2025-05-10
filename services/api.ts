583dab453135c68625b3117f6bcb86dde2129e62
Rayan Khyare <rayankhyare@gmail.com>
2025-05-10 16:22:41 +0200

    feat: added movieCards

--- services/api.ts
+++ services/api.ts
@@ -22,6 +22,3 @@ export const fetchMovies = async ({query}: {query: string} ) =>{
     const data = await response.json();
     return data.results;
 }
-
-
-// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
