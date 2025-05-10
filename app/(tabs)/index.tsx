97df7ab13bcbd8887ac5702e53423d2c72327025
Rayan Khyare <rayankhyare@gmail.com>
2025-05-10 11:18:36 +0200

    feat: fetched movies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,28 @@
+import SearchBar from '@/components/SearchBar'
+import { icons } from '@/constants/icons'
+import { images } from '@/constants/images'
+import { useRouter } from 'expo-router'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, ScrollView, View } from 'react-native'
+const Index = () => {
+  const router = useRouter();
 
-const index = () => {
+
   return (
-    <View className='flex-1 items-center justify-center'>
-      <Text className='text-5xl font-bold text-primary'>Welcome!</Text>
-    </View>
+  <View className='flex-1 bg-primary'>
+    <Image source={images.bg} className='absolute z-0 w-full' /> 
+
+    <ScrollView className='flex-1 px-5' showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight: "100%", paddingBottom: 100}}>
+      <Image source={icons.logo} className='w-12 h-10 mt-20 mb-5 mx-auto' />
+      <View className='flex-1 mt-5'>
+        <SearchBar
+          onPress={() => router.push('/search')}
+          placeholder='Search for a movie'
+        />
+      </View>
+    </ScrollView>
+  </View>
   )
 }
 
-export default index
-
-const styles = StyleSheet.create({})
+export default Index
